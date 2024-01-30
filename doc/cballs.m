a7b4ad431e3c5fbfaaa98df63d251e2ce7a14911
rodriguezmeza <marioalberto.rodriguez@inin.gob.mx>
2024-01-30 11:21:34 -0600

    Update man

--- doc/cballs.m
+++ doc/cballs.m
@@ -52,16 +52,16 @@ This will run the \fBcballs\fR code with default values and when it finish you w
 [a: search] is the searching method to use. Default is "tree-omp-sincos". Other search options will be added in the addons folder.
 
 .IP "\fBinfile\fR" 12
-[a: in] will give the name of file to input the analysed data.
+[a: in]  gives the name of file to input the data to analyse.
 
 .IP "\fBtestmodel\fR" 12
-[a: tstmodel] is the name of the model of data to create.
+[a: tstmodel] is the name of the model of data to create. By default creates a box of random points.
 
 .IP "\fBmChebyshev\fR" 12
 [a: mcheb] is the total number of chebyshev multipoles to use.
 
 .IP "\fBoutfile\fR" 12
-[a: o] will give the name of file to save the analysed data. The output is written in column form by default.
+[a: o] will give the name of file to save the analysed data. The output is written in column form by default, there is also a binary format. And by default this options is empty then no output file is saved.
 
 .IP "\fBnbody\fR" 12
 is the total number of bodies to make as a test case. By default a box of size given by \fBlengthBox\fR is created and there \fBnbody\fR random points are generated. The random generator is initialize with a seed given by \fBseed\fR option.
@@ -78,14 +78,8 @@ gives the number of bins to create.
 .IP "\fBrangeN\fR" 12
 is the maximum separtion of bodies to search for sampling.
 
-.IP "\fBstatefile\fR" 12
-[a: state] is the name of file to save the state of the run. The output is written in binary format. The frequency is given by option \fBstepState\fR.
-
 .IP "\fBstepState\fR" 12
-gives the frequency to save the whole state of the run that is save in a file given by \fBstatefile\fR in the "root" directory (usually named "Outputs").
-
-.IP "\fBrestorefile\fR" 12
-[a: restore] is the name of file to restore the state of the run.
+gives the frequency to save the the number of pivot computed. Useful to see the state of the run.
 
 .IP "\fBverbose\fR" 12
 [a: verb] to print messages to stdout. There are three levels of information. When is "0", no information is written.
@@ -97,33 +91,11 @@ gives the frequency to save the whole state of the run that is save in a file gi
 [a: opt] you may give here various code behavior options.
 
 .SH EXAMPLES
-time ../cballs nbody=100000 rangeN=500 sizeHistN=100 lbox=10000 verb=2 mToPlot=2
+time ../cballs nbody=100000 rangeN=500 sizeHistN=100 lbox=10000 verb=2.
 
 .sp
 You will have computation written in a file named with option \fBoutfile\fR in a directory named Output. If this directory does not exist, it will be created.
-Also will be created a directory named "tmp" where a log file will be saved.
-You may plot the solutions using \fBnplot2d\fR code:
-
-.sp
-nplot2d in=Output/output.txt ws=1 symbolsize=0.1 pj=0 symboltype=4 uc=1:2
-
-.sp
-\fBnplot2d\fR is very similar to gnuplot. Then, you can use the one you have available.
-
-.sp
-Other files can be plotted as:
-
-.sp
-nplot2d in=Output/histN.txt,Output_to-compare-with/histN_direct.txt uc=1:2,1:2
-
-.sp
-nplot2d in=Output/mhistZeta.txt,Output/mhistZeta.txt,Output/mhistZeta.txt,Output/mhistZeta.txt,Output/mhistZeta.txt uc=1:2,1:3,1:4,1:5,1:6
-
-.sp
-Using brute force:
-
-.sp
-time cballs o=output options=compute-HistN verb=2 sizeHistN=40 sizeHistTheta=40 rangeN=200 nbody=10000 mToPlot=2 lbox=2000 search=direct-simple-3pcf-omp
+Also will be created a directory named "tmp" in the folder Output, where a log file will be saved.
 
 .sp
 Reading a Takahasi simulation data file:
@@ -138,40 +110,6 @@ The with the output file we run cballs:
 time cballs in=Input/takas_zs9.txt options=compute-HistN verb=2 sizeHistN=40 rangeN=0.02 search=tree-omp
 
 .sp
-Other OMP routines:
-
-.sp
-time cballs o=output options=compute-HistN verb=2 nbody=64000 search=tree-omp
-
-.sp
-time cballs o=output options=compute-HistN verb=2 nbody=64000 search=tree-omp-sincos
-
-.sp
-time cballs o=output options=compute-HistN verb=2 nbody=64000 search=direct-omp
-
-.sp
-time cballs o=output options=compute-HistN verb=2 nbody=64000 search=direct-omp-sincos
-
-.sp
-Using python scripts you can generate several plots:
-
-.sp
-cd python
-
-.sp
-python plotting_points.py
-
-.sp
-python plotting_histN_histXi2pcf.py
-
-.sp
-python3 plotting_zetaM.py
-
-.sp
-To smooth a data points (reducing its number) do:
-
-.sp
-cballs o=output ofmt=columns-ascii options=smooth verb=2 nbody=64000 nsmooth=4
 
 .sp
 To convert a catalog from 2D to 3D assuming 2D column format where theta is the first column and phi the second column do:
